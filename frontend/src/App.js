a8c0facb1b34d8fe1ea123b998611aff86dbc669
shahrukh <shahrukh.aalam@ranosys.com>
2022-09-27 11:39:58 +0530

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="#">Amazona</a>
+      </header>
+      <main>List Product change 2.0</main>
+    </div>
+  );
+}
+
+export default App;
